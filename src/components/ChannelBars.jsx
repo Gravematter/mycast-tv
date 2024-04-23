c0c11a413dc3cb668b0ec95930a6b5a6ca20f6fd
SpicySpagett <166318260+SpicySpagett@users.noreply.github.com>
2024-04-23 19:09:14 -0400

    keyboard navigation
    Added keyboard navigation to the channel bars

--- src/components/ChannelBars.jsx
+++ src/components/ChannelBars.jsx
@@ -1,18 +1,87 @@
-import { React } from 'react'
+import { React, useState, useRef, useEffect } from 'react'
 
 function ChannelBars({ channels, changeChannel, updatePreview }){
+    
+    const [activeChannel, setActiveChannel] = useState(0);
+    const [newEpisode, setNewEpisode] = useState("");
+    const focusPoint = useRef(null);
+    const handleKeyDown = (e) => {
+        if(e.key === 'ArrowRight') {
+            moveFocusRight();
+        };
+        if(e.key === 'ArrowLeft') {
+            moveFocusLeft();
+        };
+        if(e.key === 'ArrowUp') {
+            moveFocusUp();
+        };
+        if(e.key === 'ArrowDown') {
+            moveFocusDown();
+        };
+        if(e.key === 'Enter') {
+            changeChannel(newEpisode);
+        };
+    };
+    const moveFocusRight = () => {
+        const listItems = document.querySelector(("#channel" + activeChannel)).childNodes;
+        const activeItem = document.activeElement;
+        for(let i = 0; i < listItems.length; i++) {
+            const listLength = listItems.length
+           if(activeItem === listItems[i] && activeItem !== listItems[listLength - 1]) {
+                listItems[i + 1].focus();
+           };
+        };
+    };
+    const moveFocusLeft = () => {
+        const listItems = document.querySelector(("#channel" + activeChannel)).childNodes;
+        const activeItem = document.activeElement;
+        for(let i = 0; i < listItems.length; i++) {
+           if(activeItem === listItems[i] && activeItem !== listItems[0]) {
+                listItems[i - 1].focus();
+           };
+        };
+    };
+    const moveFocusUp = () => {
+        const listItems = document.querySelector(("#channel" + activeChannel)).childNodes;
+        const nextChanItems = document.querySelector(("#channel" + Math.max(activeChannel - 1, 0))).childNodes;
+        const activeItem = document.activeElement;
+        for(let i = 0; i < listItems.length; i++) {
+           if(activeItem === listItems[i]) {
+            nextChanItems[i].focus();
+           };
+        };
+    };
+    const moveFocusDown = () => {
+        const listItems = document.querySelector(("#channel" + activeChannel)).childNodes;
+        const nextChanItems = document.querySelector(("#channel" + (activeChannel + 1))).childNodes;
+        const activeItem = document.activeElement;
+        for(let i = 0; i < listItems.length; i++) {
+           if(activeItem === listItems[i]) {
+            nextChanItems[i].focus();
+           };
+        };
+    };
+
     const channelData = channels.map(
-        (row, i)=>{return(
-            <tr key={i} className='channel' tabIndex={row.id} onDoubleClick={() => changeChannel(row.eplocation)}>
+        (row, i) => {return(
+            <tr key={i} tabIndex={i} className='channel' id={"channel" + i} onKeyDown={handleKeyDown} onFocus={() => {setActiveChannel(i); setNewEpisode(row.eplocation)}} onDoubleClick={() => changeChannel(row.eplocation)}>
                 <td className='channelbar'><h2>{row.name}</h2></td>
                 {row.playlist.map(
-                    (episode, j)=>{return(
-                        <td key={j} colSpan={episode.timeslots} className='episode' tabIndex={row.id} onClick={() => updatePreview(episode.title, episode.timespace, episode.description)}><h2>{episode.title}</h2></td>
+                    (episode, j) => {return(i + j === 0 ?
+                        <td key={j} tabIndex={j} ref={focusPoint} colSpan={episode.timeslots} className='episode' onFocus={() => updatePreview(episode.title, episode.timespace, episode.description)}><h2>{episode.title}</h2></td>
+                        :
+                        <td key={j} tabIndex={j} colSpan={episode.timeslots} className='episode' onFocus={() => updatePreview(episode.title, episode.timespace, episode.description)}><h2>{episode.title}</h2></td>
                     )}
                 )}
             </tr>
         )}
     );
+
+    useEffect(() => {
+        if (focusPoint.current) {
+            focusPoint.current.focus();
+        }
+    }, []);
 
     return channelData;
 }
